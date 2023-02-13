1991640bd84528e536d6b434b94ccdcd3a5ada18
Samuel Dorismond <samuel.dorismond@yahoo.com>
2023-02-13 19:35:39 +0100

    first commit

---
+++ Animation.js
@@ -0,0 +1,20 @@
+// effet d'affichage en javascript.
+const ratio = .1
+const options = {
+	root: null,
+	rootMargin: '0px',
+	threshold: ratio
+}
+
+const handleIntersect = function(entries, observer){
+	entries.forEach(function (entry){
+		if (entry.intersectionRatio > ratio){
+			entry.target.classList.add('reveal-visible')
+		};
+	});
+};
+
+const observer = new IntersectionObserver(handleIntersect, options)
+document.querySelectorAll('[class*="reveal-"]').forEach(function(r){
+	observer.observe(r)
+});
---
+++ README.md
@@ -0,0 +1,6 @@
+<h3>Nom_Projet : Wedding-Website</h3>
+<h3>Date : 11/2021</h3>
+<h3>Description : Ceci est une description de test</h3>
+<h3>Langages_Utilisés : HTML, CSS, JS</h3>
+<h3>Lien_Projet : https://samuel-dd07.github.io/Wedding-Website/</h3>
+<h3>Background : https://images.unsplash.com/photo-1550005809-91ad75fb315f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1738&q=80</h3>
